019389d7995794fd5a60c33446bfd7178d62630e
marianom23 <81484301+marianom23@users.noreply.github.com>
2022-05-13 19:15:06 -0300

    Add files via upload

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Instrumentos} from './components/Instrumentos'
+import {InstrumentoDetalle} from './components/InstrumentoDetalle'
+import {Home} from './components/Home'
+import {DondeEstamos} from './components/DondeEstamos'
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+   <BrowserRouter>
+      <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/productos' element={<Instrumentos/>}/>
+      <Route path='/producto/:id' element={<InstrumentoDetalle/>}/>
+      <Route path='/dondeEstamos' element={<DondeEstamos/>}/>
+    </Routes>
+   </BrowserRouter> 
+  )
+}
+
+export default App;
